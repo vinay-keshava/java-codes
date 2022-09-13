460eb727e472a7daf16102df7e34ed4b46631fd3
vinay-keshava <its.vicky30@gmail.com>
2022-09-13 17:43:44 +0530

    maven-archtype-webapp template project

---
+++ maven-webapp-archetype-project/src/main/java/org/hcl/SampleServlet.java
@@ -0,0 +1,17 @@
+package org.hcl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 PrintWriter pw=response.getWriter();
+		 pw.print("Hello From Servlet");
+	}
+
+}
